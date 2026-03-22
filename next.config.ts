79ba46925b91fd85a38279362546fa191a2c58d4
hermansusandy <40739755+hermansusandy@users.noreply.github.com>
2026-03-22 07:31:30 +0700

    fix: use simplest possible Dockerfile and next start for NAS compatibility

--- next.config.ts
+++ next.config.ts
@@ -3,7 +3,6 @@ import type { NextConfig } from "next";
 const nextConfig = {
   /* config options here */
   reactCompiler: true,
-  output: "standalone",
   eslint: {
     ignoreDuringBuilds: true,
   },
